3a521de42acdecd3eb1c6a4dc2af328fd13eeade
codejudge-admin <75307778+codejudge-admin@users.noreply.github.com>
2023-04-27 20:43:46 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  title = 'cj-app';
+
+  constructor() {
+    console.log(environment.apiUrl);
+  }
+}
